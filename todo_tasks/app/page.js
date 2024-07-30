5bdffd3096176df75981d00b6e3e21f908620259
CoolShivas <shivas2710cool00@gmail.com>
2024-07-31 00:54:45 +0530

    Formation of ToDo Task Form with Btn.

--- todo_tasks/app/page.js
+++ todo_tasks/app/page.js
@@ -1,5 +1,57 @@
 const HomePage = () => {
-  return <h1> Welcome to the Home Page of Next ToDo Tasks. </h1>;
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center ">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-8 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            ToDo Tasks
+          </h2>
+        </div>
+
+        <div className="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-2">
+            <div>
+              <div className="mt-2">
+                <input
+                  id="title"
+                  name="title"
+                  type="title"
+                  required
+                  autoComplete="title"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-5"
+                  placeholder="enter your title"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between"></div>
+              <div className="mt-2">
+                <input
+                  id="description"
+                  name="description"
+                  type="description"
+                  required
+                  autoComplete="description"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-5"
+                  placeholder="enter your description"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Add Task
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
